a5ba82375c71eeb8105341388bb31e1bd490d858
Hefesto23 <vini.ra@outlook.com>
2019-07-30 14:20:38 +0100

    Objets and Mutations with GraphQl

--- src/main.js
+++ src/main.js
@@ -1,25 +1,29 @@
-import { GraphQLServer } from 'graphql-yoga';
+import { GraphQLServer, PubSub } from 'graphql-yoga'
+import db from './db'
+import Query from './resolvers/Query'
+import Mutation from './resolvers/Mutation'
+import Subscription from './resolvers/Subscription'
+import User from './resolvers/User'
+import Post from './resolvers/Post'
+import Comment from './resolvers/Comment'
 
-// Type definitions (schema) 
-const typeDefs = `
-    type Query {
-        hello: String!
-    }
-`;
-
-// Resolvers
-const resolvers = {
-    Query: {
-        hello() {
-            return 'Hello world!';
-        }
-    }
-};
+const pubsub = new PubSub()
 
 // Server
 const server = new GraphQLServer({
-    typeDefs,
-    resolvers
+    typeDefs: './src/typedefs.graphql',
+    resolvers: {
+        Query,
+        Mutation,
+        Subscription,
+        User,
+        Post,
+        Comment
+    },
+    context: {
+        db,
+        pubsub
+    }
 });
 
 server.start(
