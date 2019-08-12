4b4542ea7916f3f5f4ecd96ef68133946ebea1cc
Hefesto23 <vini.ra@outlook.com>
2019-08-12 12:01:23 +0100

    Adding prisma to subscriptions and removing db.js

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -46,21 +46,6 @@ const Query = {
     },
     comments(parent, args, { prisma }, info) {
         return prisma.query.comments(null,info)
-    },
-    me() {
-        return {
-            id: '123098',
-            name: 'Mike',
-            email: 'mike@example.com'
-        }
-    },
-    post() {
-        return {
-            id: '092',
-            title: 'GraphQL 101',
-            body: '',
-            published: false
-        }
     }
 }
 
