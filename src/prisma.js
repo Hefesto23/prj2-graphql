ef75318c0802b8df71079af6203f49e97643814d
Hefesto23 <vini.ra@outlook.com>
2019-08-12 17:12:00 +0100

    Adding password and secret to prisma

--- src/prisma.js
+++ src/prisma.js
@@ -6,6 +6,7 @@ config();
 const prisma = new Prisma({
     typeDefs: 'src/generated/prisma.graphql',
     endpoint: `${process.env.GRAPHQL_HOST}:${process.env.GRAPHQL_PORT}`,
+    secret: `${process.env.PRISMA_TOKEN}`
 })
 
 export { prisma as default }
